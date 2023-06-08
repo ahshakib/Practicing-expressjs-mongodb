cba1a91b74c6912bebbb8f704d6d84dd78e73328
ahshakib <ahshakib75@gmail.com>
2023-06-08 19:43:18 +0600

    1st june express,mongodb,jwt class

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 app.use(bodyParser.json());
 const port = process.env.PORT;
 
@@ -28,6 +31,7 @@ const userSchema = mongoose.Schema(
     name: String,
     email: String,
     age: Number,
+    password: String
   },
   {
     timestamps: true,
@@ -51,7 +55,15 @@ let uid = 0;
 
 app.post("/users", async (req, res) => {
   try {
-    let user = new User(req.body);
+    const salt = await bcrypt.genSalt(10)
+    const password = await bcrypt.hash(req.body.password, salt)
+    const userObj = {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+      password: password
+    }
+    let user = new User(userObj);
     await user.save();
     res.status(201).json(user);
   } catch (error) {
@@ -59,6 +71,36 @@ app.post("/users", async (req, res) => {
     res.status(500).json({ message: "Something is wrong!" });
   }
 });
+
+//! login user
+
+app.post('/users/login', async (req, res) => {
+  try {
+
+    const {email, password} = req.body
+    const user = await User.findOne({email: email})
+
+    if(user) {
+      const isValidPassword = await bcrypt.compare(password, user.password)
+
+      if(isValidPassword) {
+        const token = jwt.sign({email: user.email, id: user._id}, 'secret')
+        const userObj = user.toJSON()
+        userObj['accessToken'] = token
+        res.status(200).json(userObj)
+        console.log('Login Successful!')
+      } else {
+        res.status(401).json({msg: 'Wrong Password'})
+      }
+    } else {
+      res.status(404).json("404 user not found");
+    }
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something is wrong!" });    
+  }
+})
 
 //! get all users
 
