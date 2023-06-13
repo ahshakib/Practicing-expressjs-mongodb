1e61ec2fdf910c2f054eb82ef3b8547cc53d67a8
ahshakib <ahshakib75@gmail.com>
2023-06-13 14:15:51 +0600

    11th june class/working with jwt refresh token and expired time of token

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 app.use(bodyParser.json());
 const port = process.env.PORT;
@@ -31,7 +31,7 @@ const userSchema = mongoose.Schema(
     name: String,
     email: String,
     age: Number,
-    password: String
+    password: String,
   },
   {
     timestamps: true,
@@ -55,14 +55,14 @@ let uid = 0;
 
 app.post("/users", async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10)
-    const password = await bcrypt.hash(req.body.password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
     const userObj = {
       name: req.body.name,
       email: req.body.email,
       age: req.body.age,
-      password: password
-    }
+      password: password,
+    };
     let user = new User(userObj);
     await user.save();
     res.status(201).json(user);
@@ -74,63 +74,63 @@ app.post("/users", async (req, res) => {
 
 //! login user
 
-app.post('/users/login', async (req, res) => {
+app.post("/users/login", async (req, res) => {
   try {
+    const { email, password, type, refreshToken } = req.body;
 
-    const {email, password} = req.body
-    const user = await User.findOne({email: email})
+    if (type == "email") {
+      await handleEmailLogin(email, password, res);
 
-    if(user) {
-      const isValidPassword = await bcrypt.compare(password, user.password)
-
-      if(isValidPassword) {
-        const token = jwt.sign({email: user.email, id: user._id}, process.env.JWT_SECRET)
-        const userObj = user.toJSON()
-        userObj['accessToken'] = token
-        res.status(200).json(userObj)
-        console.log('Login Successful!')
-      } else {
-        res.status(401).json({msg: 'Wrong Password'})
-      }
     } else {
-      res.status(404).json("404 user not found");
+      handleRefreshTokenLogin(refreshToken, res);
     }
 
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Something is wrong!" });    
+    res.status(500).json({ message: "Something is wrong!" });
   }
-})
+});
 
 //! middleware to authenticate JWT access tokens
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers.authorization
-  const token = authHeader && authHeader.split(' ')[1]
-  if(token){
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-      if(err) {
-        res.status(401).json('Unauthorized')
+      if (err) {
+        res.status(401).json("Unauthorized");
       } else {
-        req.user = user
-        next()
+        req.user = user;
+        next();
       }
-    })
-  } else{
-    res.status(401).json('Unauthorized')
-    return
+    });
+  } else {
+    res.status(401).json("Unauthorized");
+    return;
   }
-}
+};
 
 //? get user profile
 
-app.get('/profile', authenticateToken, (req, res) => {
-  res.json({user: req.user})
-})
+app.get("/profile", authenticateToken, async (req, res) => {
+  try {
+    const id = req.user.id;
+    const user = await User.findById(id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json("404 user not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something is wrong!" });
+  }
+});
 
 //! get all users
 
-app.get("/users", async (req, res) => {
+app.get("/users", authenticateToken, async (req, res) => {
   try {
     const user = await User.find({});
     res.json(user);
@@ -142,7 +142,7 @@ app.get("/users", async (req, res) => {
 
 //! get an user by id
 
-app.get("/users/:id", async (req, res) => {
+app.get("/users/:id", authenticateToken, async (req, res) => {
   const user = await getUserById(req);
 
   try {
@@ -159,7 +159,7 @@ app.get("/users/:id", async (req, res) => {
 
 //! update an user by id
 
-app.put("/users/:id", async (req, res) => {
+app.put("/users/:id", authenticateToken, async (req, res) => {
   try {
     const id = req.params.id;
     const body = req.body;
@@ -177,7 +177,7 @@ app.put("/users/:id", async (req, res) => {
 
 //! delete an user by id
 
-app.delete("/users/:id", async(req, res) => {
+app.delete("/users/:id", authenticateToken, async (req, res) => {
   try {
     const uid = req.params.id;
     const user = await User.findByIdAndDelete(uid);
@@ -191,6 +191,61 @@ app.delete("/users/:id", async(req, res) => {
     res.status(500).json({ message: "Something is wrong!" });
   }
 });
+
+function handleRefreshTokenLogin(refreshToken, res) {
+  if (refreshToken) {
+    jwt.verify(refreshToken, process.env.JWT_SECRET, async (err, payload) => {
+      if (err) {
+        res.status(401).json({ msg: 'Unauthorized' });
+      } else {
+        const id = payload.id;
+        const user = await User.findById(id);
+
+        if (!user) {
+          res.status(401).json({ msg: 'Unautorized' });
+        } else {
+          getUsersToken(user, res);
+        }
+      }
+    });
+  } else {
+    res.status(401).json({ msg: "refreshToen is not defined!" });
+  }
+}
+
+async function handleEmailLogin(email, password, res) {
+  const user = await User.findOne({ email: email });
+
+  if (user) {
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (isValidPassword) {
+      getUsersToken(user, res);
+    } else {
+      res.status(401).json({ msg: "Wrong Password" });
+    }
+  } else {
+    res.status(404).json("404 user not found");
+  }
+}
+
+function getUsersToken(user, res) {
+  const accessToken = jwt.sign(
+    { email: user.email, id: user._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "2m" }
+  );
+  const refreshToken = jwt.sign(
+    { id: user._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "3m" }
+  );
+  const userObj = user.toJSON();
+  userObj["accessToken"] = accessToken;
+  userObj["refreshToken"] = refreshToken;
+  res.status(200).json(userObj);
+  console.log("Login Successful!");
+}
 
 function getUserById(req) {
   const uid = req.params.id;
