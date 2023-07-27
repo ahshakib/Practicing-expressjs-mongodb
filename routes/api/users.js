889b229784146adaf70ca05d609f9bc0fa29240d
ahshakib <ahshakib75@gmail.com>
2023-07-28 04:10:23 +0600

    working on express-validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,42 +2,66 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
 const authenticateToken = require("../../middleware/auth");
-const User = require('../../models/User')
+const User = require("../../models/User");
 
-router.post("/", async (req, res) => {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const password = await bcrypt.hash(req.body.password, salt);
-    const userObj = {
-      name: req.body.name,
-      email: req.body.email,
-      age: req.body.age,
-      password: password,
-    };
-    let user = new User(userObj);
-    await user.save();
-    res.status(201).json(user);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Something is wrong!" });
-  }
-});
-
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password, type, refreshToken } = req.body;
-
-    if (type == "email") {
-      await handleEmailLogin(email, password, res);
-    } else {
-      handleRefreshTokenLogin(refreshToken, res);
+router.post(
+  "/",
+  [
+    body("name", "Name is required!").notEmpty(),
+    body("email", "Please enter valid email").notEmpty().isEmail(),
+    body("age", "Age is required!").optional().isNumeric(),
+    body("password", "Password with Minimum 6 character!").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const password = await bcrypt.hash(req.body.password, salt);
+      const userObj = {
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age,
+        password: password,
+      };
+      let user = new User(userObj);
+      await user.save();
+      res.status(201).json(user);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Something is wrong!" });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Something is wrong!" });
   }
-});
+);
+
+router.post(
+  "/login",
+  [
+    body("type", "Type is required!").notEmpty(),
+    body("type", "Type must be email or refresh").isIn(["email", "refresh"]),
+  ],
+  async (req, res) => {
+    try {
+      const { email, password, type, refreshToken } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      if (type == "email") {
+        await handleEmailLogin(email, password, res);
+      } else {
+        handleRefreshTokenLogin(refreshToken, res);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Something is wrong!" });
+    }
+  }
+);
 
 router.get("/", authenticateToken, async (req, res) => {
   try {
@@ -116,8 +140,6 @@ router.delete("/:id", authenticateToken, async (req, res) => {
   }
 });
 
-
-
 module.exports = router;
 
 async function handleEmailLogin(email, password, res) {
@@ -140,13 +162,13 @@ function handleRefreshTokenLogin(refreshToken, res) {
   if (refreshToken) {
     jwt.verify(refreshToken, process.env.JWT_SECRET, async (err, payload) => {
       if (err) {
-        res.status(401).json({ msg: 'Unauthorized' });
+        res.status(401).json({ msg: "Unauthorized" });
       } else {
         const id = payload.id;
         const user = await User.findById(id);
 
         if (!user) {
-          res.status(401).json({ msg: 'Unautorized' });
+          res.status(401).json({ msg: "Unautorized" });
         } else {
           getUsersToken(user, res);
         }
@@ -172,7 +194,6 @@ function getUsersToken(user, res) {
   res.status(200).json(userObj);
   console.log("Login Successful!");
 }
-
 
 function getUserById(req) {
   const uid = req.params.id;
