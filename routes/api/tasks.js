dcf7c7405157ec4e4a0511d515d2271a454d00e7
ahshakib <ahshakib75@gmail.com>
2023-08-17 02:08:44 +0600

    user can change status

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -46,25 +46,86 @@ router.get("/", authenticateToken, async (req, res) => {
   }
 });
 
-// user can edit a task
-router.put("/:id", authenticateToken, async (req, res) => {
-  try {
-    const id = req.params.id;
-    const userId = req.user.id;
-    const body = req.body;
-    const task = await Task.findOneAndUpdate({ _id: id, userId }, body, {
-      new: true,
-    });
-    if (task) {
-      res.json(task);
-    } else {
-      res.status(404).json("Task not found");
+// user can change a task status
+router.put(
+  "/status/:id",
+  [
+    authenticateToken,
+    [
+      body("status", "Status is required!").notEmpty(),
+      body("status", "Status must be to-do, in-progress, done").isIn([
+        "to-do",
+        "in-progress",
+        "done",
+      ]),
+    ],
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const id = req.params.id;
+      const userId = req.user.id;
+      const status = req.body.status;
+      const task = await Task.findOneAndUpdate(
+        { _id: id, userId },
+        { status: status },
+        {
+          new: true,
+        }
+      );
+      if (task) {
+        res.json(task);
+      } else {
+        res.status(404).json("Task not found");
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Something is wrong!" });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Something is wrong!" });
   }
-});
+);
+
+// user can change a task
+router.put(
+  "/:id",
+  [
+    authenticateToken,
+    [
+      body("status", "Status is required!").notEmpty(),
+      body("status", "Status must be to-do, in-progress, done").isIn([
+        "to-do",
+        "in-progress",
+        "done",
+      ]),
+    ],
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const id = req.params.id;
+      const userId = req.user.id;
+      const body = req.body;
+      const task = await Task.findOneAndUpdate({ _id: id, userId }, body, {
+        new: true,
+      });
+      if (task) {
+        res.json(task);
+      } else {
+        res.status(404).json("Task not found");
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Something is wrong!" });
+    }
+  }
+);
 
 router.post(
   "/login",
