15ddfd942ee4b694306db7f47893324fdd6a1459
ahshakib <ahshakib75@gmail.com>
2023-08-17 13:51:59 +0600

    file uploading with multer package

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * ! next step: run "npm i --save body-parser"
  * ? if we want to install express and body-parse with 1 command, we simply run "npm i --save express body-parser"
  * ! next step: run "npm i --save-dev nodemon"
+ * ! npm i --save multer
  */
 
 /**
@@ -17,20 +18,40 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
+const multer = require("multer");
 const connectDB = require("./config/db");
 
 app.use(bodyParser.json());
 const port = process.env.PORT;
 
+// const upload = multer({ dest: './public/uploads/' })
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    // const uniqueSuffix = 'shakib75.jpg'
+    //cb(null, file.fieldname + '-' + file.originalname)
+    cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
+
 //mongodb connection
 
-connectDB()
+connectDB();
 
 //routes
 
-app.use('/users', require('./routes/api/users'))
-app.use('/tasks', require('./routes/api/tasks'))
+app.use("/users", require("./routes/api/users"));
+app.use("/tasks", require("./routes/api/tasks"));
+
+app.post("/uploads", upload.single("file"), (req, res) => {
+  res.json({ message: "file is uploaded" });
+});
 
 
 app.listen(port, () => {
@@ -40,4 +61,3 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to my app" });
 });
-
